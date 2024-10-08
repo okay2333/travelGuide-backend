7fec68ad1818b1f991c7eac0542756a802e1e3f0
okay <138407783+okay2333@users.noreply.github.com>
2024-10-08 11:54:00 +0800

    '更新gitnore文件'

--- src/main/java/com/huang/controller/FileController.java
+++ src/main/java/com/huang/controller/FileController.java
@@ -2,6 +2,8 @@ package com.huang.controller;
 
 import cn.dev33.satoken.stp.StpUtil;
 import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.lang.UUID;
+import com.google.gson.Gson;
 import com.huang.common.BaseResponse;
 import com.huang.common.ErrorCode;
 import com.huang.common.ResultUtils;
@@ -13,9 +15,19 @@ import com.huang.exception.BusinessException;
 import com.huang.manager.CosManager;
 import com.huang.service.UserService;
 import java.io.File;
+import java.io.IOException;
 import java.util.Arrays;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+
+import com.qcloud.cos.utils.IOUtils;
+import com.qiniu.common.QiniuException;
+import com.qiniu.http.Response;
+import com.qiniu.storage.Configuration;
+import com.qiniu.storage.Region;
+import com.qiniu.storage.UploadManager;
+import com.qiniu.storage.model.DefaultPutRet;
+import com.qiniu.util.Auth;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -43,67 +55,51 @@ public class FileController {
 
     /**
      * 文件上传
-     *
-     * @param multipartFile
-     * @param uploadFileRequest
-     * @param request
+     * @param file
+     * @param prefix
      * @return
      */
     @PostMapping("/upload")
-    public BaseResponse<String> uploadFile(@RequestPart("file") MultipartFile multipartFile,
-                                           UploadFileRequest uploadFileRequest, HttpServletRequest request) {
-        String biz = uploadFileRequest.getBiz();
-        FileUploadBizEnum fileUploadBizEnum = FileUploadBizEnum.getEnumByValue(biz);
-        if (fileUploadBizEnum == null) {
-            throw new BusinessException(ErrorCode.PARAMS_ERROR);
-        }
-        validFile(multipartFile, fileUploadBizEnum);
-
-        // 文件目录：根据业务、用户来划分
-        String uuid = RandomStringUtils.randomAlphanumeric(8);
-        String filename = uuid + "-" + multipartFile.getOriginalFilename();
-        String filepath = String.format("/%s/%s/%s", fileUploadBizEnum.getValue(), StpUtil.getLoginIdAsLong(), filename);
-        File file = null;
+    public String uploadFile(MultipartFile file, String prefix) {
+        String accessKey = "Ce0HbXmIEXsmEpw_Z8LhZGqNW6QHKJaJKS36Qqe4";
+        //构造一个带指定 Region 对象的配置类，指定存储区域，和存储空间选择的区域一致
+        Configuration cfg = new Configuration(Region.autoRegion());
+        //...其他参数参考类注释
+        UploadManager uploadManager = new UploadManager(cfg);
+        //...生成上传凭证，然后准备上传
+        String secretKey = "-d_RPCSw8eUckuJgPaHSHJE2E8olsnB1mUpCjyxr";
+        String bucket =  "web-dev-oss";
+        String url =  "http://sl0pp5u7u.sabkt.gdipper.com/";
+        //默认不指定key的情况下，以文件内容的hash值作为文件名
+        String key = UUID.randomUUID().toString()+"."+prefix;
         try {
-            // 上传文件
-            file = File.createTempFile(filepath, null);
-            multipartFile.transferTo(file);
-            cosManager.putObject(filepath, file);
-            // 返回可访问地址
-            return ResultUtils.success(FileConstant.COS_HOST + filepath);
-        } catch (Exception e) {
-            log.error("file upload error, filepath = " + filepath, e);
-            throw new BusinessException(ErrorCode.SYSTEM_ERROR, "上传失败");
-        } finally {
-            if (file != null) {
-                // 删除临时文件
-                boolean delete = file.delete();
-                if (!delete) {
-                    log.error("file delete error, filepath = {}", filepath);
-                }
+
+            //得到本地文件的字节数组
+            byte[] bytes = IOUtils.toByteArray(file.getInputStream());
+            //认证
+            Auth auth = Auth.create(accessKey, secretKey);
+            //认证通过后得到token（令牌）
+            String upToken = auth.uploadToken(bucket);
+            try {
+                //上传文件,参数：字节数组，key，token令牌
+                //key: 建议我们自已生成一个不重复的名称
+                Response response = uploadManager.put(bytes, key, upToken);
+                //解析上传成功的结果
+                DefaultPutRet putRet = new Gson().fromJson(response.bodyString(), DefaultPutRet.class);
+                System.out.println(putRet.key);
+                System.out.println(putRet.hash);
+                String imageUrl = url + putRet.key;
+                return imageUrl;
+            } catch (QiniuException ex) {
+                log.error("上传文件至OSS[异常]",ex);
+
             }
+        } catch (IOException ex) {
+            log.error("上传文件至OSS[异常]",ex);
+
         }
+        return null;
     }
 
-    /**
-     * 校验文件
-     *
-     * @param multipartFile
-     * @param fileUploadBizEnum 业务类型
-     */
-    private void validFile(MultipartFile multipartFile, FileUploadBizEnum fileUploadBizEnum) {
-        // 文件大小
-        long fileSize = multipartFile.getSize();
-        // 文件后缀
-        String fileSuffix = FileUtil.getSuffix(multipartFile.getOriginalFilename());
-        final long ONE_M = 1024 * 1024L;
-        if (FileUploadBizEnum.USER_AVATAR.equals(fileUploadBizEnum)) {
-            if (fileSize > ONE_M) {
-                throw new BusinessException(ErrorCode.PARAMS_ERROR, "文件大小不能超过 1M");
-            }
-            if (!Arrays.asList("jpeg", "jpg", "svg", "png", "webp").contains(fileSuffix)) {
-                throw new BusinessException(ErrorCode.PARAMS_ERROR, "文件类型错误");
-            }
-        }
-    }
+
 }
